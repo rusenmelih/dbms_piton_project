8b0696fe0ed1ff1c126c81d1f75920cbb8c6a3ce
rusenmelih <m.rusenmelih@gmail.com>
2021-12-14 02:50:12 +0300

    Login Controller creating and bootstrap adding for f-e

---
+++ DBMS_PITON/DBMS_PITON/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBMS_PITON.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
